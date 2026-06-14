d295346ed86c83318224ba29bcbee24b8cdfe20e
Rhennan-ALT <reehsecundario@gmail.com>
2026-06-14 20:58:52 -0300

    mod: ajuste do endpoint post

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/cartas", response_model=schemas.CartaPoke)
+@app.post("/cartas", response_model=schemas.CartaPoke, status_code=201)
 def criar_card(
     card: schemas.CartaPCreate, 
     db: Session = Depends(get_db)
